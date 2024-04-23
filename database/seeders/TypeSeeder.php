996354361b5a4732f8c5de1752c67a3b8f026911
giosciafra <francescogioscia8@gmail.com>
2024-04-23 17:02:46 +0200

    feat: type seeder done

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,20 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = [
+            'back-end',
+            'front-end',
+            'database',
+            'full-stack'
+        ];
+
+        foreach ($types as $type) {
+
+            $newType = new Type();
+
+            $newType->name = $type;
+
+            $newType->save();
+        }
     }
 }
